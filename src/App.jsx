ac101b026e568906ab807218a014e3b4274c9683
Soham <jambhwadekarsoham@gmail.com>
2024-08-27 17:36:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import Profile from './Profile';
 import Register from './Register'
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+      <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
